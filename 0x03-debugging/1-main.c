46e3e49f6748d3da215c8e4d6940c564d7bec835
hadassah <kayekanem@gmail.com>
2023-04-26 05:59:10 -0700

    1mainagain

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,10 +15,10 @@ int main(void)
 	/**
 	 * while (i < 10)
 	 * {
-	 *gputchar(i);
+	 *putchar(i);
 	 * }
 	 */
-	printf("Infinite loop avoided: \\0/\n");
+	printf("Infinite loop avoided! \\0/\n");
 
 	return (0);
 }
