c5e95e28bf00dfa6737d67d9824f498df588e560
hadassah <kayekanem@gmail.com>
2023-05-02 07:46:55 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - puts a string followed by a new line
+ * @str: inputs string
+ * Return: no Return
+ */
+void _put(char *str)
+{
+	int calc = 0;
+
+	while (calc >= 0)
+	{
+		if (str[calc] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[calc]);
+		calc++;
+	}
+}
