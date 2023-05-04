ad0a370fc542f4a60a5354368cfb3f33450a5605
hadassah <kayekanem@gmail.com>
2023-05-04 14:25:52 -0700

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,77 @@
 #include "main.h"
-#include <stdio.h>
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
- * infinte_add - adds two numbers
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer
+ * @r_index: current buffer index
+ * Return: a pointer to the result
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int calc, t = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		calc = (*n1 - '0') + (*n2 - '0');
+		calc += t;
+		*(r + r_index) = (calc % 10) + '0';
+		t = calc / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		calc = (*n1 - '0') + t;
+		*(r + r_index) = (calc % 10) + '0';
+		t = calc / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		calc = (*n2 - '0') + t;
+		*(r + r_index) = (calc % 10) + '0';
+		t = calc / 10;
+	}
+	if (t && r_index >= 0)
+	{
+		*(r + r_index) = (t % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (t && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
+}
+
+/**
+ * infinite_add - adds two numbers
  * @n1: number one
  * @n2: number two
  * @r: buffer that the fuction stores the result
  * @size_r: the size of the buffer
- * Return: the pointer to dest
+ * Return: a pointer to the result
  */
-char *infinte_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int i, n1_cal = 0, n2_cal = 0;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
+	for (i = 0; *(n1 + i); i++)
+		n1_cal++;
+
+	for (i = 0; *(n2 + 1); i++)
+		n2_cal++;
+
+	if (size_r <= n1_cal + 1 || size_r <= n2_cal + 1)
 		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
-	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
-	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+
+	n1 += n1_cal - 1;
+	n2 += n2_cal - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
