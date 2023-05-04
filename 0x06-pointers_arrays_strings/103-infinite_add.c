c323356e50f8bbb6ef8db2d12106e359d618e3d5
hadassah <kayekanem@gmail.com>
2023-05-04 13:48:52 -0700

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,41 +11,41 @@
  */
 char *infinte_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a1 = 0, a2 = 0, mm, nn, k1, k2, calc = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + a1) != '\0')
-		a1++;
-	while (*(n2 + a2) != '\0')
-		a2++;
-	if (a1 >= a2)
-		nn = a1;
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		nn = a2;
-	if (size_r <= nn + 1)
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	r[nn + 1] = '\0';
-	a1--, a2--, size_r--;
-	k1 = *(n1 + a1) - 48, k2 = *(n2 + a2) - 48;
-	while (nn >= 0)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		mm = k1 + k2 + calc;
-		if (mm >= 10)
-			calc = mm / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
-			calc = 0;
-		if (mm > 0)
-			*(r + nn) = (mm % 10) + 48;
+			add = 0;
+		if (op > 0)
+			*(r + bg) = (op % 10) + 48;
 		else
-			*(r + nn) = '0';
-		if (a1 > 0)
-			a1--, k1 = *(n1 + a1) - 48;
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
-			k1 = 0;
-		if (a2 > 0)
-			a2--, k2 = *(n2 + a2) - 48;
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
-			k2 = 0;
-		nn--, size_r--;
+			dr2 = 0;
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
