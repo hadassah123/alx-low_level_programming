64609afd1e2c46a08403c42568a8387582505012
hadassah <kayekanem@gmail.com>
2023-05-04 21:37:28 -0700

    please

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,31 +9,30 @@
  */
 void print_buffer(char *b, int size)
 {
-	int m, n, l;
+	int j, k, l;
 
 	if (size <= 0)
 		printf("\n");
 	else
 	{
-		for (m = 0; m < size; m += 10)
+		for (j = 0; j < size; j += 10)
 		{
-			printf("%.8x:", m);
-
-			for (n = m; n < m + 10; n++)
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
 			{
-				if (n % 2 == 0)
+				if (k % 2 == 0)
 					printf(" ");
-				if (n < size)
-					printf("%.2x", *(b + n));
+				if (k < size)
+					printf("%.2x", *(b + k));
 				else
-					printf(" ");
+					printf("  ");
 			}
 			printf(" ");
-			for (l = m; l < m + 10; l++)
+			for (l = j; l < j + 10; l++)
 			{
 				if (l >= size)
 					break;
-				if (*(b + l) <  32 || *(b + l) > 126)
+				if (*(b + l) < 32 || *(b + l) > 126)
 					printf("%c", '.');
 				else
 					printf("%c", *(b + l));
